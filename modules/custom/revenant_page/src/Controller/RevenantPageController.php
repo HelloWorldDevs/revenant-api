b7bcf4bdf512a808508c42bd68de974ad8301b99
Keenan <k.p.wellsy@gmail.com>
2017-01-11 12:59:33 -0800

    test for post revenant page node route and function added

--- modules/custom/revenant_page/src/Controller/RevenantPageController.php
+++ modules/custom/revenant_page/src/Controller/RevenantPageController.php
@@ -11,6 +11,8 @@ use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Drupal\node\Entity\Node;
+
 class RevenantPageController extends ControllerBase {
 
     /**
@@ -20,6 +22,32 @@ class RevenantPageController extends ControllerBase {
 
         $response['data'] = 'Some test data to return';
         $response['method'] = 'GET';
+
+        return new JsonResponse( $response );
+    }
+
+    public function post_example( Request $request ) {
+
+        // This condition checks the `Content-type` and makes sure to
+        // decode JSON string from the request body into array.
+        if ( 0 === strpos( $request->headers->get( 'Content-Type' ), 'application/json' ) ) {
+            $data = json_decode( $request->getContent(), TRUE );
+            $request->request->replace( is_array( $data ) ? $data : [] );
+        }
+
+        $node = Node::create(array(
+            'type' => 'revenant_page',
+            'title' => 'NEW PAGE!',
+            'langcode' => 'en',
+            'uid' => '1',
+            'status' => 1,
+            'field_url' => 'asdfs/asdfa/asdf',
+        ));
+
+        $node->save();
+
+        $response['data'] = 'Some test data to return';
+        $response['method'] = 'POST';
 
         return new JsonResponse( $response );
     }
