1da9f4a4cfe794479890f5a903cd62c19f28ff08
Keenan <k.p.wellsy@gmail.com>
2017-01-18 13:47:21 -0800

    post controller creates page and default page content

--- webroot/modules/custom/revenant_page/src/Controller/RevenantPageController.php
+++ webroot/modules/custom/revenant_page/src/Controller/RevenantPageController.php
@@ -26,32 +26,46 @@ class RevenantPageController extends ControllerBase {
         return new JsonResponse( $response );
     }
 
-    public function post_example( Request $request ) {
+    public function post_page( Request $request ) {
 
         // This condition checks the `Content-type` and makes sure to
         // decode JSON string from the request body into array.
         if ( 0 === strpos( $request->headers->get( 'Content-Type' ), 'application/json' ) ) {
             $data = json_decode( $request->getContent(), TRUE );
             $request->request->replace( is_array( $data ) ? $data : [] );
-            \Drupal::logger('my_module')->error("here is a message");
-            $node = Node::create(array(
-                'type' => 'revenant_page',
-                'title' => $data['title'],
-                'langcode' => 'en',
-                'uid' => '1',
-                'status' => 1,
-                'field_page_url' => $data['url'],
-            ));
-
-            $node->save();
         }
+        $content = json_decode($request->getContent(), TRUE);
 
+        //create node for page on check
+        $page_node = Node::create(array(
+            'type' => 'revenant_page',
+            'title' => $content['title'],
+            'langcode' => 'en',
+            'uid' => '1',
+            'status' => 1,
+            'field_page_url' => $content['url'],
+        ));
+        $page_node->save();
+        $page_node_id = $page_node->id();
 
-        $response['data'] = 'Some test data to return';
+        // create default content node for page
+        $content_node = Node::create(array(
+            'type' => 'revenant_content_item',
+            'title' => 'default content item',
+            'langcode' => 'en',
+            'uid' => '1',
+            'status' => 1,
+            'body' => 'default content item',
+            'field_new_content' => 'default content item',
+            'field_xpath' => 'default content item',
+        ));
+        $content_node->field_page->target_id = $page_node_id;
+        $content_node->save();
+
+        $response['data'] = 'Some newer test data to return';
         $response['method'] = 'POST';
 
         return new JsonResponse( $response );
     }
-
 
 }
