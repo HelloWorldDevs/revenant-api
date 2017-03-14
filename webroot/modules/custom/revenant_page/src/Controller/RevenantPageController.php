a1d61295421dac68a7314d5952052995f35cd233
Keenan Wells <Keenan@Keenans-MacBook-Air.local>
2017-03-14 12:00:41 -0700

    switched back to http client

--- webroot/modules/custom/revenant_page/src/Controller/RevenantPageController.php
+++ webroot/modules/custom/revenant_page/src/Controller/RevenantPageController.php
@@ -47,11 +47,16 @@ class RevenantPageController extends ControllerBase {
                 'client_id' => $client_id,
                 'client_secret'=> $client_secret
             );
-            $client = new \GuzzleHttp\Client();
-            $request = $client->request('POST', '/oauth/token', [
-//                    'auth' => ['oauth2'],
-                    'form_params' => $auth_body
-                ]);
+//            $client = new \GuzzleHttp\Client();
+//            $request = $client->request('POST', 'http://revenant-api.bfdig.com/oauth/token', [
+////                    'auth' => ['oauth2'],
+//                    'form_params' => $auth_body
+//                ]);
+            $client = \Drupal::httpClient();
+            $request = $client->post('http://revenant-api.bfdig.com/oauth/token', [
+                'auth' => [$username,$password],
+                'form_params' => $auth_body
+            ]);
             $response = $request->getBody();
             $response['method'] = 'hello';
         }
