d2ed71675c5839fb977556ece83574db70cac046
Daniel Linn <danlinn@Daniels-MacBook-Pro.local>
2017-03-14 16:39:57 -0700

    add status to log

--- webroot/revenant/js/page/page.js
+++ webroot/revenant/js/page/page.js
@@ -270,7 +270,7 @@ var pageModule = (function ($) {
                     console.log('oauth error', error)
                 })
                 .done(function (response, status, xhr) {
-                    console.log(response);
+                    console.log(response, status);
                     var response_data = JSON.parse(response);
                     sessionStorage.setItem('rev_auth', JSON.stringify({
                         "username": username,
