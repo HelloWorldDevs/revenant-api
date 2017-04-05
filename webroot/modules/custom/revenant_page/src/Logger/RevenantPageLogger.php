3d6cb4664badfb59d32a85488971214380de4c6d
Keenan Wells <Keenan@Keenans-MacBook-Air.local>
2017-04-05 10:44:43 -0700

    remove logger to seperate class

---
+++ webroot/modules/custom/revenant_page/src/Logger/RevenantPageLogger.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Keenan
+ * Date: 4/5/17
+ * Time: 10:36 AM
+ */
+
+namespace Drupal\revenant_page\Logger;
+
+use Drupal\Core\Logger\RfcLoggerTrait;
+use Psr\Log\LoggerInterface;
+
+
+class RevenantPageLogger implements LoggerInterface {
+    use RfcLoggerTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function log($level, $message, array $context = array()) {
+        \Drupal::logger('revenant_page')->notice($message);
+    }
+
+}
