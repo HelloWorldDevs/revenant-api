9d450f7548fd6aa888bc7bf15f7db6ee544c15ca
Keenan Wells <Keenan@Keenans-MacBook-Air.local>
2017-04-07 22:24:44 -0700

    upload working

--- webroot/revenant/src/page/pageController.js
+++ webroot/revenant/src/page/pageController.js
@@ -45,7 +45,7 @@ var pageControllerModule = (function($){
       //ckeditor toolbar configuration
       var editor = CKEDITOR.inline(el, {
         bodyId: data,
-        extraPlugins: 'inlinesave,simpleuploads,image2',
+        extraPlugins: 'inlinesave,simpleuploads',
         filebrowserImageUploadUrl: DEV_CONFIG + 'revenant_page/page_content/image',
         postAuth: 'Bearer ' + authToken,
         allowedContent: true,
